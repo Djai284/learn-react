9639ce4c1d8db8b93c3ce8c0aa157c7eeadf925c
Dhananjai <dhananjai284@gmail.com>
2024-10-07 16:13:41 -0400

    fixed artists removal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,20 @@ import Profile from "./components/profile_props";
 import Gallery from "./qcomps/state";
 import Bio from "./qcomps/bios";
 import RecipeList from "./qcomps/recipes";
-import List from "./qcomps/list_keys_id";
+// import List from "./qcomps/list_keys_id";
 import PackingList from "./qcomps/props_item";
 import Form from "./qcomps/stuckForm";
 import FeedbackForm from './qcomps/thankYouCrash';
 import RequestTracker from './qcomps/shoppingCart';
 import Scoreboard from "./qcomps/updObjectsForm";
+import List from "./qcomps/artistsRemoveArr";
 
 export default function Home() {
   return (
     <>
       {/* <Form /> */}
       {/* <RequestTracker /> */}
-      <Scoreboard />
+      <List />
     </>
   );
 }
